86c0358af583f2c516d25d6a3454cdc39ecf7745
deepak878 <deepakbhattarai878@gmail.com>
2023-05-19 23:28:58 +0545

    done upto post delete

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,63 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [err, setErr] = useState(null);
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(inputs);
+      await axios.post("/auth/register", inputs);
+      navigate('/login')
+    } catch (error) {
+      setErr(error.response.data);
+    }
+  };
+
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Login</h1>
       <form>
-        <input required type='text' placeholder='username'></input>
-        <input required type='email' placeholder='email'></input>
-        <input required type='password' placeholder='password'></input>
-        <button>Login</button>
-        <p>This is an error</p>
-        <span>Don't you have an account? <Link to="/register">Register</Link></span>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        ></input>
+
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        ></input>
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        ></input>
+        <button onClick={handleSubmit}>Register</button>
+      { err && <p>{err}</p>}        
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
       </form>
     </div>
-  )
-}
- 
-export default Register
+  );
+};
+
+export default Register;
--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -1,33 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Menu } from '../components/Menu'
-const SinglePost = () => {
-  return (
-    <div className='single'>
-      <div className="content">
-        <img/>
-        <div className="user">
-          <img/>
-          <div className="info">
-            <span>John</span>
-            <p>Posted 2 days ago</p>
-          </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-            <img src='/images/edit.png'/>
-            </Link>
-            <img src='/images/delete.png'/>
-          </div>
-        </div>
-        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione quod dicta excepturi doloremque quas minima minus, accusantium ad odit blanditiis modi quae enim expedita, vel voluptatibus placeat porro dolor quis!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis fugiat quae numquam in, accusamus cumque! Ea perspiciatis dolorum, id impedit deserunt doloribus. <br/>
-          Illo unde ad dolorum optio explicabo asperiores totam, nemo commodi a cumque sint rem dignissimos ipsa neque ullam! Quia facere recusandae doloremque sint id fugit quidem veniam voluptate magni, modi laborum aperiam asperiores dignissimos rerum.<br/>
-           Aliquid quod architecto nostrum modi at ipsa. Ut minus illum animi aspernatur voluptatibus ducimus consequuntur nesciunt, perferendis earum aliquam ipsa ratione tempore quia cupiditate molestias accusantium quidem atque accusamus corporis dolorem inventore illo vitae culpa. Nisi, accusantium quae! Error, porro? Architecto mollitia beatae veniam neque, dolore, perspiciatis optio aut possimus dicta dolorum laborum perferendis sunt, fugiat iure dignissimos at consequatur quia ad.<br/>
-            Aliquid odio ducimus porro dignissimos in nihil, sapiente illum quis libero soluta commodi cupiditate incidunt quibusdam odit debitis quia! Fuga debitis fugiat dolore incidunt, voluptate id illum eius ipsam. Ex repellendus tempora sit nisi dolores? Atque culpa, est nisi quis ad doloremque.<br/> Sint et, aliquid ducimus quis quaerat tenetur voluptas ab quas eum pariatur magni nesciunt, minus voluptates repellat veritatis perspiciatis labore. Repellat odit at tenetur hic aspernatur odio quidem illum, aliquid perferendis nemo, ad nisi explicabo reiciendis veniam alias unde!</p>
-      </div>
-     <Menu/>
-    </div>
-  )
-}
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import axios from "axios";
+import moment from "moment";
+import { Menu } from "../components/Menu";
 
-export default SinglePost
+const SinglePost = () => {
+  const [post, setPost] = useState({});
+  const location = useLocation();
+const navigate = useNavigate()
+  const postId = location.pathname.split("/")[2];
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async()=>{
+    try {
+    await axios.delete(`/posts/${postId}`)
+    navigate("/")
+
+    } catch (error) {
+      
+    }
+
+  }
+  return (
+    <div className="single">
+      <div className="content">
+        <img src={post?.img} />
+        <div className="user">
+{ post.userImg && <img src={post.userImg} alt=""/>}
+          <div className="info">
+            <span>{post?.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
+          </div>
+          {currentUser?.username === post?.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src="/images/edit.png" />
+              </Link>
+              <img onClick={handleDelete} src="/images/delete.png" />
+            </div>
+          )}
+        </div>
+        <h1>{post.title}</h1>
+
+        {post.desc}
+      </div>
+      <Menu />
+    </div>
+  );
+};
+
+export default SinglePost;
